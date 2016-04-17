6eff351e3289c5299b392691006175e2f2e88742
jadoux <jadoux@lynkoa.com>
2016-04-17 18:27:50 +0200

    most new element

--- app/common/models/planning.js
+++ app/common/models/planning.js
@@ -1,3 +1,16 @@
 module.exports = function(Planning) {
+  Planning.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
 
+    for(var i = 0; i < req.body.length; i++) {
+      var filterWhere = {
+        "date": req.body[i].date,
+        "collaboratorId": req.body[i].collaboratorId
+      };
+      Planning.destroyAll(filterWhere, function(err, info) {
+      });
+    }
+
+    next();
+  });
 };
